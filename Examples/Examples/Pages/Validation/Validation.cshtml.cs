6d86484264d46dffdf5bf459a189267d550f000c
Michael Gfeller <mgfeller@hsr.ch>
2019-05-16 07:18:41 +0200

    added validation example

---
+++ Examples/Examples/Pages/Validation/Validation.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Examples.Pages.Validation
+{
+
+    public class Item
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 5 )]
+        public string Name { get; set; }
+    }
+    public class ValidationModel : PageModel
+    {
+        public Item Item { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost(Item item)
+        {
+            return Content(ModelState.IsValid ? "Valid" : "Invalid");
+        }
+    }
+}
