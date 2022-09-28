170842553764b2909c8cd649ae2d6180b04c3601
Michael Gfeller <michael.gfeller@ost.ch>
2022-09-28 11:59:16 +0200

    net6

--- Uebung/vorlage/BmiRechner/Startup.cs
+++ Uebung/vorlage/BmiRechner/Startup.cs
@@ -22,15 +22,12 @@ namespace BmiRechner
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+ 
         public void ConfigureServices(IServiceCollection services)
         { 
-            services.AddMvc();
-            services.AddSingleton<IBmiService, BmiService>();
+            services.AddMvc(); 
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {         
             app.UseStaticFiles();
