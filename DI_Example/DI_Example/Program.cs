8f2c5b5b30f16a478632283a5d4934405f12df60
Michael Gfeller <michael.gfeller@ost.ch>
2023-08-15 11:37:44 +0200

    update programms

--- DI_Example/DI_Example/Program.cs
+++ DI_Example/DI_Example/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DI_Example
 {
--- Middleware/Middleware/Program.cs
+++ Middleware/Middleware/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace Middleware
 {
@@ -47,9 +49,27 @@ namespace Middleware
 
             app.UseMiddleware<RequestLoggerMiddleware>();*/
 
-
-
             app.Run();
         }
     }
+
+    public class RequestLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestLoggerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestLoggerMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation("Handling request: " + context.Request.Path);
+            await _next.Invoke(context);
+            _logger.LogInformation("Finished handling request.");
+        }
+    }
+
 }
