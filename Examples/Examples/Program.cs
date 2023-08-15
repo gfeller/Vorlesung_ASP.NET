c8163037a4a60b1ee0116cae787cedb588905e96
Michael Gfeller <michael.gfeller@ost.ch>
2023-08-15 11:51:55 +0200

    small fixes

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using Examples.Services;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Examples
 {
@@ -21,39 +18,17 @@ namespace Examples
             builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
             builder.Services.AddSession(options => { options.IdleTimeout = TimeSpan.FromMinutes(15); });
-
             builder.Services.AddSingleton<CitiesService, CitiesService>();
-            builder.Services.AddServerSideBlazor();
+            
 
             // Add services to the container.
-
             var app = builder.Build();
-
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-
-                app.UseHsts(); // warum macht dies Sinn?
-            }
-
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
             app.UseSession();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapRazorPages();
-            });
-
+            app.MapRazorPages();
 
             app.Run();
         }
